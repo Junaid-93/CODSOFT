ccae8c37c1374b2138414382c88927d868115099
Junaid <engr.hja@gmail.com>
2023-09-24 04:37:08 +0500

    To-Do App completed

--- To-do app/main.js
+++ To-do app/main.js
@@ -1,49 +1,56 @@
-
+// Tasks Array on local storage
 const tasksArray = localStorage.getItem("tasks")
   ? JSON.parse(localStorage.getItem("tasks"))
   : [];
 
 console.log(tasksArray);
 
+// Function for Task creation
+function createTask(task) {
+    tasksArray.push(task.value);
+    localStorage.setItem("tasks", JSON.stringify(tasksArray));
+    location.reload();
+}
+
+// Task Creation Logic
 document.querySelector("#addBtn").addEventListener("click", () => {
 const task = document.querySelector("#task");
 createTask(task);
 });
 
-function createTask(task) {
-  tasksArray.push(task.value);
-  localStorage.setItem("tasks", JSON.stringify(tasksArray));
-  location.reload();
-}
 
+// Function to Display Tasks
 function displayTasks() {
   let todos = "";
   for (let i = 0; i < tasksArray.length; i++) {
-    todos += `<div class="task">
-            <div class="task-section">
-                <textarea disabled>${tasksArray[i]}</textarea>
-                <div class="edit-section">
-                    <i class="fa-solid fa-pen-to-square edit"></i>
-                    <i class="fa-solid fa-trash delete"></i>
-                </div>
-            </div>
-            <div class="updation">
-                <button class="save">Save</button>
-                <button class="cancel">Cancel</button>
-            </div>
-        </div>`;
+    todos +=   `<div class="task">
+                    <div class="task-section">
+                        <textarea disabled>${tasksArray[i]}</textarea>
+                        <div class="edit-section">
+                            <i class="fa-solid fa-pen-to-square edit"></i>
+                            <i class="fa-solid fa-trash delete"></i>
+                        </div>
+                    </div>
+                    <div class="updation" style="display:none">
+                        <button class="save">Save</button>
+                    </div>
+                </div>`;
   }
   document.querySelector("#todos").innerHTML = todos;
 
-  activateDeleteListeners();
-  activateEditListeners();
-  activateSaveListeners();
-  activateCancelListeners();
-
-
+  deleteListener();
+  editListener();
+  saveListener();
 }
 
-function activateDeleteListeners() {
+// Function to Delete Tasks
+function deleteTask(task) {
+    tasksArray.splice(task, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasksArray));
+    location.reload();
+  }
+
+function deleteListener() {
   let deleteBtn = document.querySelectorAll(".delete");
   deleteBtn.forEach((db, task) => {
     db.addEventListener("click", () => {
@@ -53,13 +60,8 @@ function activateDeleteListeners() {
 }
 
 
-function deleteTask(task) {
-  tasksArray.splice(task, 1);
-  localStorage.setItem("tasks", JSON.stringify(tasksArray));
-  location.reload();
-}
-
-function activateEditListeners() {
+// Function to Edit Tasks
+function editListener() {
   const editBtn = document.querySelectorAll(".edit");
   const updation = document.querySelectorAll(".updation");
   const inputs = document.querySelectorAll(".task-section textarea");
@@ -71,31 +73,21 @@ function activateEditListeners() {
   });
 }
 
-function activateSaveListeners() {
+// Function to update task
+function updateTask(text, i) {
+    tasksArray[i] = text;
+    localStorage.setItem("tasks", JSON.stringify(tasksArray));
+    location.reload();
+  }
+
+// Function to save task after updation
+function saveListener() {
   const saveBtn = document.querySelectorAll(".save");
   const inputs = document.querySelectorAll(".task-section textarea");
   saveBtn.forEach((sb, task) => {
     sb.addEventListener("click", () => {
       updateTask(inputs[task].value, task);
 
-    });
-  });
-}
-
-function updateTask(text, i) {
-  tasksArray[i] = text;
-  localStorage.setItem("tasks", JSON.stringify(tasksArray));
-  location.reload();
-}
-
-function activateCancelListeners() {
-  const cancelBtn = document.querySelectorAll(".cancel");
-  const updateController = document.querySelectorAll(".updation");
-  const inputs = document.querySelectorAll(".task-section textarea");
-  cancelBtn.forEach((cb, task) => {
-    cb.addEventListener("click", () => {
-      updateController[task].style.display = "none";
-      inputs[task].disabled = true;
     });
   });
 }
