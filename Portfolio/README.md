e319f56a8ca8c3da9e3752509b1a2fe77a907777
Junaid <engr.hja@gmail.com>
2023-09-26 07:24:03 +0500

    Minor Fixes

--- Portfolio/README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio
-Static Personal Portfolio
--- blogging-platform/Backend/config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const DBconnection = () => {
-    mongoose.connect(process.env.MONGODB_URL)
-    .then ( () => console.log("Database is connected"))
-    .catch( () => console.log("Dabase connection failed"))
-}
-
-module.exports = DBconnection
